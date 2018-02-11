6ec71bdf4b4414780e27cc761a76903fb0e52c64
Alexandra Ulsh <ulsh@mapbox.com>
2018-02-11 00:11:27 -0500

    Moves JavaScript to separate map.js file

---
+++ map.js
@@ -0,0 +1,237 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWx1bHNoIiwiYSI6ImY0NDBjYTQ1NjU4OGJmMDFiMWQ1Y2RmYjRlMGI1ZjIzIn0.pngboKEPsfuC4j54XDT3VA';
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-77.04, 38.89],
+    zoom: 10.60
+});
+
+const cabiBikeScale = [
+  [0, '#F2F12D'],
+  [10, '#EED322'],
+  [20, '#E6B71E'],
+  [50, '#DA9C20'],
+  [100, '#CA8323'],
+  [200, '#B86B25'],
+  [300, '#A25626'],
+  [400, '#8B4225'],
+  [500, '#723122']
+]
+
+const jumpScale = [
+  [0, '#F2F12D'],
+  [1, '#EED322'],
+  [2, '#E6B71E'],
+  [3, '#DA9C20'],
+  [4, '#CA8323'],
+  [5, '#B86B25'],
+  [6, '#A25626'],
+  [7, '#8B4225'],
+  [8, '#723122']
+]
+
+function BikeShareStation (lon, lat, capacity, stationId, bikesAvailable) {
+  this.type = 'Feature',
+  this.geometry = {},
+  this.properties = {},
+  this.geometry.type = 'Point',
+  this.geometry.coordinates = [lon, lat],
+  this.properties.capacity = capacity
+  this.properties.stationId = stationId
+  this.properties.bikesAvailable = bikesAvailable
+}
+
+function FeatureCollection (features) {
+  this.type = 'FeatureCollection',
+  this.features = features
+}
+
+function cabiData() {
+  let stationArray = [];
+
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json');
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+      const stations = request.response.data.stations;
+      stations.forEach(station => {
+        let newStation = new BikeShareStation(station.lon, station.lat, station.capacity);
+        stationArray.push(newStation);
+      });
+      resolve(stationArray);
+    }
+  });
+}
+
+function jumpData(){
+  let bikeArray = [];
+
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://dc.jumpmobility.com/opendata/free_bike_status.json');
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+      const bikes = request.response.data.bikes;
+      bikes.forEach(bike => {
+        let lonLat = [];
+        lonLat.push(bike.lon);
+        lonLat.push(bike.lat);
+        bikeArray.push(lonLat);
+      });
+      resolve(bikeArray);
+    }
+  });
+}
+
+function loadDcNeighborhoods(bikeshareData, jumpData) {
+
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://opendata.arcgis.com/datasets/f6c703ebe2534fc3800609a07bad8f5b_17.geojson');
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    const cabiStations = new FeatureCollection(bikeshareData);
+    const neighborhoods = request.response.features;
+    const jumpBikes = turf.points(jumpData);
+    
+    neighborhoods.forEach(neighborhood => {
+      const polygon = turf.polygon(neighborhood.geometry.coordinates);
+      const cabiWithin = turf.pointsWithinPolygon(cabiStations, polygon);
+      const jumpWithin = turf.pointsWithinPolygon(jumpBikes, polygon);
+
+      let totalBikes = 0;
+      cabiWithin.features.forEach(station =>{
+        totalBikes += station.properties.capacity;
+      });
+
+      neighborhood.properties.cabiBikes = totalBikes;
+      neighborhood.properties.jumpBikes = jumpWithin.features.length;
+
+      map.addLayer({
+        id: `cabibikes-${neighborhood.properties.OBJECTID}`,
+        type: 'fill',
+        source: {
+          type: 'geojson',
+          data: neighborhood
+        },
+        layout: {
+          visibility: 'none'
+        },
+        paint: {
+          'fill-color': {
+            property: 'cabiBikes',
+            stops: cabiBikeScale
+          },
+          'fill-opacity': 0.6,
+          'fill-outline-color': '#FFF'
+        }
+      });
+
+      map.addLayer({
+        id: `jumpbikes-${neighborhood.properties.OBJECTID}`,
+        type: 'fill',
+        source: {
+          type: 'geojson',
+          data: neighborhood
+        },
+        layout: {
+          visibility: 'none'
+        },
+        paint: {
+          'fill-color': {
+            property: 'jumpBikes',
+            stops: jumpScale
+          },
+          'fill-opacity': 0.6,
+          'fill-outline-color': '#FFF'
+        }
+      });
+
+      var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+
+      map.on('mouseenter', `cabibikes-${neighborhood.properties.OBJECTID}`, function (e) {
+        popup.setLngLat(e.lngLat)
+          .setHTML(`<h4>${e.features[0].properties.NBH_NAMES}</h4><p>${e.features[0].properties.cabiBikes} Capital Bikeshare bikes</p>`)
+          .addTo(map);
+      });
+
+      map.on('mouseenter', `jumpbikes-${neighborhood.properties.OBJECTID}`, function (e) {
+        popup.setLngLat(e.lngLat)
+          .setHTML(`<h4>${e.features[0].properties.NBH_NAMES}</h4><p>${e.features[0].properties.jumpBikes} JUMP bikes</p>`)
+          .addTo(map);
+      });
+
+      map.on('mouseleave', `cabibikes-${neighborhood.properties.OBJECTID}`, function() {
+        popup.remove();
+      });
+
+      map.on('mouseleave', `jumpbikes-${neighborhood.properties.OBJECTID}`, function() {
+        popup.remove();
+      });
+    });
+  };
+}
+
+var toggles = [ ['Capital Bikeshare Bikes','cabibikes'], 
+  ['JUMP Bikes','jumpbikes']];
+
+toggles.forEach(toggle => {
+  var link = document.createElement('a');
+  link.href = '#';
+  link.className = 'active';
+  link.id = toggle[1];
+  link.textContent = toggle[0];
+
+  link.onclick = function (e) {
+    var clickedLayer = this.id;
+    e.preventDefault();
+    e.stopPropagation();
+
+    var legend = document.getElementById(`${clickedLayer}-legend`);
+    var visibility = map.getLayoutProperty(`${clickedLayer}-1`, 'visibility');
+
+    if (visibility === 'visible') {
+      legend.style.display = "none";
+      for (var i = 1; i < 47; i++) {
+        map.setLayoutProperty(`${clickedLayer}-${i}`, 'visibility', 'none');
+        this.className = '';
+      }
+    } else {
+      legend.style.display = "";
+      for (var i = 1; i < 47; i++) {
+        map.setLayoutProperty(`${clickedLayer}-${i}`, 'visibility', 'visible');
+        this.className = '';
+      }
+    }
+  };
+
+  var layers = document.getElementById('menu');
+  layers.appendChild(link);
+
+});
+
+map.on('load', function () {
+
+  var bikeshareData;
+  var jumpBikeData;
+
+  var bikeshare = cabiData()
+    .then(res => bikeshareData = res)
+    .catch(err => console.error(err))
+
+  var jumpBike = jumpData()
+    .then(res => jumpBikeData = res)
+    .catch(err => console.error(err))
+
+  Promise.all([bikeshare, jumpBike]).then(function(res){
+    loadDcNeighborhoods(res[0],res[1]);
+  });
+
+});
