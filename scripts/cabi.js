a5be187c5c8dfea682de501744eaed0f75767862
Alexandra Ulsh <alexandra.ulsh@gmail.com>
2021-04-02 14:39:43 -0400

    Fixes eslint and prettier issues in cabi.js and jump.js

--- scripts/cabi.js
+++ scripts/cabi.js
@@ -1,32 +1,40 @@
-const request = require('request');
-const turf = require('@turf/turf');
+/* eslint-disable no-console */
 
-const dcNeighborhoodData = 'https://opendata.arcgis.com/datasets/f6c703ebe2534fc3800609a07bad8f5b_17.geojson';
-const capitalBikeshareData = 'https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json';
+const request = require("request");
+const turf = require("@turf/turf");
+
+const dcNeighborhoodData =
+  "https://opendata.arcgis.com/datasets/f6c703ebe2534fc3800609a07bad8f5b_17.geojson";
+const capitalBikeshareData =
+  "https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json";
 
 function BikeShareStation(lon, lat, capacity) {
-  this.type = 'Feature',
-  this.geometry = {},
-  this.properties = {},
-  this.geometry.type = 'Point',
-  this.geometry.coordinates = [lon, lat],
+  this.type = "Feature";
+  this.geometry = {};
+  this.properties = {};
+  this.geometry.type = "Point";
+  this.geometry.coordinates = [lon, lat];
   this.properties.capacity = capacity;
 }
 
 function FeatureCollection(features) {
-  this.type = 'FeatureCollection',
+  this.type = "FeatureCollection";
   this.features = features;
 }
 
-function bikeShareArray(){
-  let stationArray = [];
+function bikeShareArray() {
+  const stationArray = [];
 
   return new Promise((resolve, reject) => {
     request(capitalBikeshareData, (error, response, body) => {
       if (error) reject(error);
-      const stations = JSON.parse(body).data.stations;
-      stations.forEach(station => {
-        let newStation = new BikeShareStation(station.lon, station.lat, station.capacity);
+      const { stations } = JSON.parse(body).data;
+      stations.forEach((station) => {
+        const newStation = new BikeShareStation(
+          station.lon,
+          station.lat,
+          station.capacity
+        );
         stationArray.push(newStation);
       });
       resolve(stationArray);
@@ -40,19 +48,21 @@ function loadDcNeighborhoods(bikeshareData) {
     const neighborhoods = JSON.parse(body).features;
     const stations = new FeatureCollection(bikeshareData);
 
-    neighborhoods.forEach(neighborhood => {
+    neighborhoods.forEach((neighborhood) => {
       const polygon = turf.polygon(neighborhood.geometry.coordinates);
       const ptsWithin = turf.pointsWithinPolygon(stations, polygon);
 
       let totalBikes = 0;
-      ptsWithin.features.forEach(station =>{
+      ptsWithin.features.forEach((station) => {
         totalBikes += station.properties.capacity;
       });
-      console.log(`${ptsWithin.features.length} Capital Bikeshare stations with ${totalBikes} bikes in ${neighborhood.properties.NBH_NAMES}`);
+      console.log(
+        `${ptsWithin.features.length} Capital Bikeshare stations with ${totalBikes} bikes in ${neighborhood.properties.NBH_NAMES}`
+      );
     });
   });
 }
 
 bikeShareArray()
-  .then(res => loadDcNeighborhoods(res))
-  .catch(err => console.error(err));
+  .then((res) => loadDcNeighborhoods(res))
+  .catch((err) => console.error(err));
--- scripts/jump.js
+++ scripts/jump.js
@@ -1,18 +1,21 @@
-const request = require('request');
-const turf = require('@turf/turf');
+/* eslint-disable no-console */
 
-const dcNeighborhoodData = 'https://opendata.arcgis.com/datasets/f6c703ebe2534fc3800609a07bad8f5b_17.geojson';
-const jumpData = 'https://gbfs.uber.com/v1/dcb/free_bike_status.json';
+const request = require("request");
+const turf = require("@turf/turf");
 
-function jumpArray(){
-  let bikeArray = [];
+const dcNeighborhoodData =
+  "https://opendata.arcgis.com/datasets/f6c703ebe2534fc3800609a07bad8f5b_17.geojson";
+const jumpData = "https://gbfs.uber.com/v1/dcb/free_bike_status.json";
+
+function jumpArray() {
+  const bikeArray = [];
 
   return new Promise((resolve, reject) => {
     request(jumpData, (error, response, body) => {
       if (error) reject(error);
-      const bikes = JSON.parse(body).data.bikes;
-      bikes.forEach(bike => {
-        let lonLat = [];
+      const { bikes } = JSON.parse(body).data;
+      bikes.forEach((bike) => {
+        const lonLat = [];
         lonLat.push(bike.lon);
         lonLat.push(bike.lat);
         bikeArray.push(lonLat);
@@ -22,20 +25,22 @@ function jumpArray(){
   });
 }
 
-function loadDcNeighborhoods(jumpData) {
+function loadDcNeighborhoods(jumpBikeData) {
   request(dcNeighborhoodData, (error, response, body) => {
     if (error) console.log(error);
     const neighborhoods = JSON.parse(body).features;
-    const jumpBikes = turf.points(jumpData);
+    const jumpBikes = turf.points(jumpBikeData);
 
-    neighborhoods.forEach(neighborhood => {
+    neighborhoods.forEach((neighborhood) => {
       const polygon = turf.polygon(neighborhood.geometry.coordinates);
       const ptsWithin = turf.pointsWithinPolygon(jumpBikes, polygon);
-      console.log(`${ptsWithin.features.length} JUMP bikes in ${neighborhood.properties.NBH_NAMES}`);
+      console.log(
+        `${ptsWithin.features.length} JUMP bikes in ${neighborhood.properties.NBH_NAMES}`
+      );
     });
   });
 }
 
 jumpArray()
-  .then(res => loadDcNeighborhoods(res))
-  .catch(err => console.error(err));
+  .then((res) => loadDcNeighborhoods(res))
+  .catch((err) => console.error(err));
