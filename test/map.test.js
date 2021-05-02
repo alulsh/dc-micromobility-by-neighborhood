5406e30dabf1cdb2dd380dd7a9d2ceb65200c839
Alexandra Ulsh <alexandra.ulsh@gmail.com>
2021-05-02 14:13:19 -0400

    Adds currently failing test for live API call

--- test/map.test.js
+++ test/map.test.js
@@ -1,9 +1,15 @@
 import { expect, test } from "@jest/globals";
-import { convertToGeoJSON } from "../map";
+import { convertToGeoJSON, getCabiStationInformation } from "../map";
 import { cabiStationInformationMock, cabiStationGeoJSON } from "./fixtures";
 
 test("Converts Capital Bikeshare JSON to valid GeoJSON", () => {
   expect(convertToGeoJSON(cabiStationInformationMock.data.stations)).toEqual(
     cabiStationGeoJSON
   );
+});
+
+test("Requests Capital Bikeshare station information", () => {
+  return getCabiStationInformation().then((data) => {
+    expect(data).toBe(cabiStationGeoJSON);
+  });
 });
